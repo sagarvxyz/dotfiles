7e2b829e9ade7a1e5f0a02b1935efcc49c61bd44
Sagar Velagala <svelagala@netflix.com>
2025-04-14 10:20:03 -0400

    fix script permissions

---
+++ git/functions.zsh
@@ -0,0 +1,16 @@
+# Git functions
+
+function git_branch_delete_matching() {
+	if [ -z "$1" ]; then
+		echo "Usage: git_branch_delete_matching <pattern>"
+		return 1
+	fi
+
+	targets=$(git branch | grep $1)
+	if [ -z "$targets" ]; then
+		echo "No branches found matching '$1'"
+		return 1
+	fi
+
+	git branch -d $targets
+}
