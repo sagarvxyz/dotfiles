17dd3814cdca7b8210cfd1fab284e4274fddfe0c
Sagar Velagala <svelagala@netflix.com>
2025-04-17 07:23:57 -0400

    update git settings

--- git/functions.zsh
+++ git/functions.zsh
@@ -14,3 +14,17 @@ function git_branch_delete_matching() {
 
 	git branch -d $targets
 }
+
+function git_branch_clean() {
+  # Fetch the latest changes from the remote
+  git fetch --prune
+
+  # Get a list of local branches that have no upstream
+  local branches_to_delete=$(git branch -vv | awk '/: gone]/{print \$1}')
+
+  # Iterate over each branch and delete it
+  for branch in $branches_to_delete; do
+    echo "Deleting branch: $branch"
+    git branch -d $branch
+  done
+}
