e9313558ffd18103e7e8cf3e092858d789e2c7a8
Sagar Velagala <sagar.velagala@gmail.com>
2025-04-19 09:17:27 -0400

    setup personal dotfiles

---
+++ git/aliases.zsh
@@ -0,0 +1,12 @@
+#!/bin/bash
+alias gl='git pull --prune'
+alias gc='git commit'
+alias gca='git commit -a'
+alias gco='git checkout'
+alias gb='git branch'
+alias gbdgrep='git_branch_delete_matching'
+alias gbc='git branch-prune'
+alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gac='git add -A && git commit -m'
+alias gz='git_undo'
+
