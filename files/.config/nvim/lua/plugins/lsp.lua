161aaf0d2d73527790821b92a555ac8453afa4f6
Sagar Velagala <sagar.velagala@gmail.com>
2025-07-21 07:43:03 -0400

    add amp

--- files/.config/nvim/lua/plugins/lsp.lua
+++ files/.config/nvim/lua/plugins/lsp.lua
@@ -57,6 +57,12 @@ return {
 				virtual_text = false,
 				signs = {
 					severity = { min = vim.diagnostic.severity.HINT },
+					text = {
+						[vim.diagnostic.severity.ERROR] = "✘",
+						[vim.diagnostic.severity.WARN] = "▲",
+						[vim.diagnostic.severity.HINT] = "⚑",
+						[vim.diagnostic.severity.INFO] = "»",
+					},
 				},
 				underline = true,
 				update_in_insert = false,
@@ -70,13 +76,6 @@ return {
 					prefix = "",
 				},
 			})
-
-			-- Diagnostic signs
-			local signs = { Error = "✘", Warn = "▲", Hint = "⚑", Info = "»" }
-			for type, icon in pairs(signs) do
-				local hl = "DiagnosticSign" .. type
-				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-			end
 
 			-- Auto-popup diagnostics on cursor hold
 			vim.api.nvim_create_autocmd("CursorHold", {
